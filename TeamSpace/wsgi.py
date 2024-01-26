062ca89f083d1026086f857c49a702e2254095c7
Ruslan Kobrynchuk <ruslankobrynchuk@MacBook-Air-Ruslan.local>
2024-01-26 13:36:42 +0200

    init commit

---
+++ TeamSpace/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for TeamSpace project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TeamSpace.settings')
+
+application = get_wsgi_application()
