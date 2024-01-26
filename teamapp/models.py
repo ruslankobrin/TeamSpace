5075f18207d6bd3571ed0d31390da93ff7d515ac
ruslankobrynchuk <ruslankobrynchuk@gmail.com>
2024-01-26 14:16:02 +0200

    add model Person, Team

--- teamapp/models.py
+++ teamapp/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    teams = models.ManyToManyField('Team', related_name='members')
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
