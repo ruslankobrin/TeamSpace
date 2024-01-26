377b8da7ba3c5ccb6b25c2f80230134a05f4f65a
ruslankobrynchuk <ruslankobrynchuk@gmail.com>
2024-01-26 17:42:27 +0200

    make remove method

--- teamapp/views.py
+++ teamapp/views.py
@@ -9,15 +9,28 @@ class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
 
-    @action(detail=True, methods=["post"], serializer_class=TeamAllSerializer)
-    def move_to_team(self, request, pk=None):
+    def check_args(self, request):
         person = self.get_object()
         team_id = request.data.get("team_id")
         try:
             team = Team.objects.get(id=team_id)
         except Team.DoesNotExist:
             return Response({"error": "Team not found"}, status=400)
+        return person, team
+
+    @action(detail=True, methods=["post"], serializer_class=TeamAllSerializer)
+    def move_person_to_team(self, request, pk=None):
+        person, team = self.check_args(request)
+
         person.teams.add(team)
+        serializer = PersonSerializer(self.get_object())
+        return Response(serializer.data)
+
+    @action(detail=True, methods=["post"], serializer_class=TeamAllSerializer)
+    def remove_person_to_team(self, request, pk=None):
+        person, team = self.check_args(request)
+
+        person.teams.remove(team)
         serializer = PersonSerializer(self.get_object())
         return Response(serializer.data)
 
