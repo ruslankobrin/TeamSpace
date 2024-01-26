99ad9e95157ec86b70a425e8893fd25da10c9087
ruslankobrynchuk <ruslankobrynchuk@gmail.com>
2024-01-26 13:55:14 +0200

    add requirements.txt, install database

--- TeamSpace/settings.py
+++ TeamSpace/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'TeamSpace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'teamspace',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
